02d68afe6a40c26eee4e7e16d61c84d6abbec7fe
timetam <timetam24@naver.com>
2023-06-17 16:53:05 +0900

    Handling Login to My Website

--- login_function/app.js
+++ login_function/app.js
@@ -21,54 +21,64 @@ pd.setAttribute("type","password");
 pd.setAttribute("placeholder","비밀번호");
 pd.classList.add("pd");
 
-const submit_btn = document.createElement("input");
-submit_btn.setAttribute("type","submit");
-submit_btn.setAttribute("value","로그인");
-submit_btn.setAttribute("disabled", "");
-submit_btn.classList.add("disabled_btn");
+const error_message = document.createElement("p");
+error_message.classList.add("error_message");
+
+const app_login_btn = document.createElement("input");
+app_login_btn.setAttribute("type","button");
+app_login_btn.setAttribute("value","로그인");
+app_login_btn.setAttribute("disabled", "");
+app_login_btn.classList.add("disabled_btn");
 
 const login_btn_wrapper = document.getElementById("login_btn_wrapper");
 
 id_pd_wrapper.append(id,pd);
-form.append(id_pd_wrapper, submit_btn, login_btn_wrapper);
+form.append(id_pd_wrapper, error_message, app_login_btn, login_btn_wrapper);
 form_wrapper.append(form);
 header.appendChild(h1);
 root.append(header, form_wrapper);
 
 const idAndPdEmptyCheckHandler = () => {
     if(id.value.trim() !== '' && pd.value.trim() !== '' ){
-        submit_btn.removeAttribute("disabled");
-        submit_btn.className = 'submit_btn';
+        app_login_btn.removeAttribute("disabled");
+        app_login_btn.className = 'app_login_btn';
     }else{
-        submit_btn.setAttribute("disabled", "");
-        submit_btn.className = 'disabled_btn';
+        app_login_btn.setAttribute("disabled", "");
+        app_login_btn.className = 'disabled_btn';
     }
 };
 
-const submitBtnHandler = (event) => {
-    event.preventDefault()
-
-    const idCheck = /^[a-zA-Z]+[a-zA-Z0-9]{5,19}$/;
+const loginBtnHandler = () => {
+    const idCheck = /^[a-zA-Z]+(?=.*[a-zA-Z])(?=.*[0-9]).{5,19}$/;
     // 영문자로 시작하는 영문자 또는 숫자 6~20자리 아이디 입력
     const pdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
     // 영문자+숫자+특수문자 조합으로 8~25자리 비밀번호 입력
 
 if(!idCheck.test(id.value)){
-    alert("아이디는 영문자로 시작해야 하며 영문자, 숫자 조합으로 6~20자리 이내여야 합니다.");
+    error_message.innerText = "아이디는 영문자로 시작해야 하며 영문자, 숫자 조합으로 6~20자리 이내여야 합니다.";
     id.focus();
     return false;
 }
 
 if(!pdCheck.test(pd.value)){
-    alert("비밀번호는 영문자, 숫자, 특수문자 조합으로 8~25자리 이내여야 합니다.");
+    error_message.innerText = "비밀번호는 영문자, 숫자, 특수문자 조합으로 8~25자리 이내여야 합니다.";
     pd.focus();
     return false;
 }
 
-// form.submit();
+if(id.value !== "imuser1" || pd.value !== "frontend1@"){
+    error_message.innerText = "아이디 또는 비밀번호가 일치하지 않습니다.";
+    pd.focus();
+    return false;
+}
+
+if(id.value === "imuser1" && pd.value === "frontend1@"){
+    error_message.innerText = "";
+    window.location.href = "http://127.0.0.1:5500/login_function/app_login_success.html";
+}
 };
 
 id.addEventListener("input", idAndPdEmptyCheckHandler);
 pd.addEventListener("input", idAndPdEmptyCheckHandler);
 
-submit_btn.addEventListener("click", submitBtnHandler);
+app_login_btn.addEventListener("click", loginBtnHandler);
