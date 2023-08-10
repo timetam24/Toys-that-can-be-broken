f68c0f0c1896c654167a15400fcd7806d792f79f
timetam <timetam24@naver.com>
2023-08-10 14:39:23 +0900

    개봉예정영화 페이지 D-Day 계산 기능 추가

--- movie_ticket/app.js
+++ movie_ticket/app.js
@@ -1,4 +1,6 @@
 const menuLinks = document.querySelectorAll(".menu-item");
+const openingDays = document.querySelectorAll("time");
+const movieCards = document.querySelectorAll(".release-container");
 
 const currentMenuTextBold = () => {
   const currentPageTopic = document
@@ -11,6 +13,25 @@ const currentMenuTextBold = () => {
   });
 };
 
+const putDDayInsideMovieCard = () => {
+  const ddayArray = [];
+
+  const today = new Date();
+  [...openingDays].forEach((day) => {
+    const openDay = new Date(`${day.dateTime} 00:00:00`);
+    const diffDay = Math.floor((openDay - today) / (1000 * 60 * 60 * 24)) + 1;
+    ddayArray[ddayArray.length] = diffDay;
+  });
+
+  movieCards.forEach((card, index) => {
+    const $span = document.createElement("span");
+    $span.className = "d-day";
+    $span.innerText = `D-${ddayArray[index]}`;
+    card.appendChild($span);
+  });
+};
+
 window.onload = function () {
   currentMenuTextBold();
+  putDDayInsideMovieCard();
 };
