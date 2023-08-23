1c686281e86a3970a9bce36d850941c02834f0b0
timetam <timetam24@naver.com>
2023-08-23 16:06:39 +0900

    콜라콜라 자판기 초기작업

---
+++ vending_machine/app/data.tsx
@@ -0,0 +1,10 @@
+let data = [
+  { id: 1, name: "Original_Cola", price: 1000, src: "/cola-original.png" },
+  { id: 2, name: "Violet_Cola", price: 1000, src: "/cola-violet.png" },
+  { id: 3, name: "Yellow_Cola", price: 1000, src: "/cola-yellow.png" },
+  { id: 4, name: "Cool_Cola", price: 1000, src: "/cola-cool.png" },
+  { id: 5, name: "Green_Cola", price: 1000, src: "/cola-green.png" },
+  { id: 6, name: "Orange_Cola", price: 1000, src: "/cola-orange.png" },
+];
+
+export default data;
---
+++ vending_machine/app/page.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import data from "./data";
+
+export default function Home() {
+  return (
+    <>
+      <h1>
+        <Image
+          className="logo"
+          src="/cola-logo.png"
+          alt="Cola Cola Logo"
+          width={386}
+          height={180}
+          priority
+        />
+      </h1>
+      <main>
+        <section className="section1">
+          <h2 className="a11y-hidden">판매음료</h2>
+          <ul className="cola-list">
+            {data.map((cola) => (
+              <li key={cola.id}>
+                <button type="button" className="btn-cola">
+                  <Image
+                    src={cola.src}
+                    alt={cola.name}
+                    width={72}
+                    height={130}
+                  />
+                  <span className="cola-name">{cola.name}</span>
+                  <strong className="cola-price">{cola.price}원</strong>
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className="container">
+            <div className="bg-box">
+              <h2 className="box-title">잔액 :</h2>
+              <p>원</p>
+            </div>
+            <button type="button" className="btn">
+              거스름돈 반환
+            </button>
+
+            <label htmlFor="input-money" className="a11y-hidden">
+              입금액
+            </label>
+            <input
+              type="number"
+              id="input-money"
+              min="1000"
+              step="1000"
+              placeholder="입금액 입력"
+              required
+            />
+            <button type="button" className="btn">
+              입금
+            </button>
+
+            <h2 className="a11y-hidden">장바구니</h2>
+            <ul className="get-list"></ul>
+            <button type="button" className="btn-get">
+              획득
+            </button>
+          </div>
+        </section>
+        <section className="section2">
+          <div className="bg-box">
+            <h2 className="box-title">소지금 :</h2>
+            <p>25,000원</p>
+          </div>
+        </section>
+        <section className="section3">
+          <h2>획득한 음료</h2>
+          <ul className="get-list"></ul>
+          <p className="total-price">총금액 : 9,000원</p>
+        </section>
+      </main>
+    </>
+  );
+}
